7144ab37dc9273b9e9123a2a2568ecb846759a90
AnnaYurova <94358663+AnnaYurova@users.noreply.github.com>
2022-02-23 23:56:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,57 +4,59 @@
 
 
 bool checkPrime(uint64_t value) {
-  uint64_t x = 1;
-    while(value % x == 0){
-      if((value = x))
-        return true;
-      else
+  int count = 0;
+    for (int i = 1; i < 8; i++) {
+      if (value % i == 0) {
+        count++;
+      }
+      if(count>2)
         return false;
     }
-  }
+  return true;
+}
+
 
 uint64_t nPrime(int a[], uint64_t n) {
   int count = 0;
   int rez =0;
-  for( int i=0; i< n; ++i){
-      for( int j=1; j<10000; ++j){
-        if(a[i]%j==0){
-            count ++;
+  for (int i = 0; i < n; ++i) {
+      for (int j = 1; j < 10000; ++j) {
+        if (a[i]%j == 0) {
+            count++;
         }
       }
-        if(count < 3){
-            rez =a[i];
+        if (count < 3) {
+            rez = a[i];
         }
-      count =0;
+      count = 0;
     }
   return rez;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  value ++;
-    for (int i=2; i<8;i++){
-      if(value % i==0){
-       	value ++;
-      }
-      else 
+  value++;
+    for (int i = 2; i < 8; i++) {
+      if (value % i == 0) {
+        value++;
+      }else
         return value;
     }
-  }
-
+  return value;
+}
 
 uint64_t sumPrime(uint64_t hbound) {
   int count = 0;
   int sum = 0;
-    for( int i=1; i< hbound; ++i){
-      for( int j=1; j<hbound; ++j){
-        if(i%j==0){
-          count ++;
+    for (int i = 1; i < hbound; ++i) {
+      for (int j = 1; j < hbound; ++j) {
+        if (i % j == 0) {
+          count++;
         }
       }
-        if(count < 3){
-          sum+=i;
+        if (count < 3) {
+          sum += i;
         }
-        count =0;
+        count = 0;
       }
     return sum;
   }
