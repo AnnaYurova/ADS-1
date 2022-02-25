2372b9cdf7f19e291cb34d3944d33bab488d2d2b
AnnaYurova <94358663+AnnaYurova@users.noreply.github.com>
2022-02-25 13:12:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -34,17 +34,16 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-  while(1){
-    int c=1;
+  while (1) {
+    int c = 1;
     value++;
-      for(int i=2;i<=value;i++)
-        if(value%i==0)
+      for (int i = 2; i <= value; i++)
+        if (value % i == 0)
           c++;
         if (c==2)
           return value;
-   }
+  }
 }
-
 
 uint64_t sumPrime(uint64_t hbound) {
   int count = 0;
